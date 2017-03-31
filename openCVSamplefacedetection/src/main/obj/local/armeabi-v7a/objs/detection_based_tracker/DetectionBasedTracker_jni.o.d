a2c663494ad29e1a33b6cc60d8d79a6f0de243d9
luoyu <303132289@qq.com>
2017-03-31 23:11:46 +0800

    基于opencv的android人脸识别
    eclipse项目移植AS，gradle还在配置中

---
+++ openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,50 @@
+E:/Android_project/face-detection/openCVSamplefacedetection/src/main/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+  E:/Android_project/face-detection/openCVSamplefacedetection/src/main/jni/DetectionBasedTracker_jni.cpp \
+  E:/Android_project/face-detection/openCVSamplefacedetection/src/main/jni\DetectionBasedTracker_jni.h \
+  ../../sdk/native/jni/include\opencv2/core/core.hpp \
+  ../../sdk/native/jni/include\opencv2/core/types_c.h \
+  ../../sdk/native/jni/include\opencv2/core/version.hpp \
+  D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm \
+  D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath \
+  D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef \
+  D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex \
+  D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\map \
+  D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector \
+  ../../sdk/native/jni/include\opencv2/core/operations.hpp \
+  D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\limits \
+  ../../sdk/native/jni/include\opencv2/core/mat.hpp \
+  ../../sdk/native/jni/include\opencv2/contrib/detection_based_tracker.hpp \
+  ../../sdk/native/jni/include\opencv2/objdetect/objdetect.hpp \
+  D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque
+
+E:/Android_project/face-detection/openCVSamplefacedetection/src/main/jni\DetectionBasedTracker_jni.h:
+
+../../sdk/native/jni/include\opencv2/core/core.hpp:
+
+../../sdk/native/jni/include\opencv2/core/types_c.h:
+
+../../sdk/native/jni/include\opencv2/core/version.hpp:
+
+D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\algorithm:
+
+D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cmath:
+
+D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\cstddef:
+
+D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\complex:
+
+D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\map:
+
+D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\vector:
+
+../../sdk/native/jni/include\opencv2/core/operations.hpp:
+
+D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\limits:
+
+../../sdk/native/jni/include\opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include\opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include\opencv2/objdetect/objdetect.hpp:
+
+D:/sdk/sdk/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\deque:
